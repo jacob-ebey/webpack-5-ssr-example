12aafdd472678b74ddfcd7d85e24d44fdfaf4016
Jacob Ebey <jacob.ebey@live.com>
2020-04-05 22:05:56 -0700

    Added apollo-client (#3)
    * Added apollo-client
    
    * Added babel plugin for graphql-tag
    
    * Update .gitignore

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Helmet } from 'react-helmet-async'
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/react-hooks'
+
+const QUERY = gql`
+  query HomePageQuery {
+    allFilms {
+      films {
+        id
+        title
+        releaseDate
+      }
+    }
+  }
+`
 
 function HomePage () {
+  const { data, loading } = useQuery(QUERY)
+
   return (
     <>
       <Helmet>
@@ -12,6 +28,29 @@ function HomePage () {
       <div className='home'>
         <h1>Hello, World!</h1>
         <Link to='/about'>About Page</Link>
+
+        {!data && loading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <p>
+              <a
+                href='https://github.com/graphql/swapi-graphql'
+                rel='noopener noreferrer'
+                target='_blank'
+              >
+                SWAPI
+              </a>
+              {' '}
+              data loaded with apollo-client
+            </p>
+            <ul>
+              {data.allFilms.films.map(film => (
+                <li key={film.id}>{film.title} was published {film.releaseDate}</li>
+              ))}
+            </ul>
+          </>
+        )}
       </div>
     </>
   )
